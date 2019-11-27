3c8c903260273d656c8ba130896238eae2c2ef94
Injung Chung <mu29@yeoubi.net>
2019-11-27 16:54:41 +0900

    fix: 너비가 주어지지 않은 경우 100%로 고정되던 오류를 수정한다

--- src/composer/Grid.ts
+++ src/composer/Grid.ts
@@ -1,7 +1,7 @@
 import { css } from 'styled-components';
 import { media } from '../internal';
 
-const calculateWidth = (value?: number) => `${(value || 12) / 12 * 100}%`
+const calculateWidth = (value?: number) => value ? `${(value || 12) / 12 * 100}%` : undefined;
 
 export interface GridProps {
   sm?: number;
@@ -15,10 +15,10 @@ export const Grid = css<GridProps>`
   `}
 
   ${media.md`
-    width: ${props => calculateWidth(props.md)};
+    width: ${props => calculateWidth(props.md || props.sm)};
   `}
 
   ${media.lg`
-    width: ${props => calculateWidth(props.lg)};
+    width: ${props => calculateWidth(props.lg || props.md || props.sm)};
   `}
 `;
